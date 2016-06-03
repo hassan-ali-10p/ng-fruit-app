a70151c99778bf3dba1357c0f747057d61b6b64c
hassan-ali-10p <hassan.ali@tenpearls.com>
2016-06-03 15:14:59 +0500

    adding and removing an element in the list

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 angular.module('app', [])
   .controller('appCtrl', function($scope){
     $scope.menu = 'Fruit App';
-
+    $scope.name;
+    $scope.price;
+    var count = 3;  // because already three elements are added in fruits list
     $scope.fruits = [
-      {"name": "apple", "price": "64"},
-      {"name": "orange", "price": "63"},
-      {"name": "pineApple", "price": "21"}
+      {"id": "1", "name": "apple", "price": "64"},
+      {"id": "2", "name": "orange", "price": "63"},
+      {"id": "3", "name": "pineApple", "price": "21"}
     ];
+
+    $scope.addFruit = function(fruit={}){
+      count++;
+      fruit["id"] = count;
+      $scope.fruits.push(fruit);
+    };
+    $scope.deleteFruit = function(fruitId){
+      for (var i=$scope.fruits.length-1; i>=0; i--) {
+          if ($scope.fruits[i].id === fruitId) {
+              $scope.fruits.splice(i, 1);
+          }
+      }
+    };
   });
