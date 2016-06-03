b765029dd5a4c4c53a45ab49adcaa09c131ff0ba
hassan-ali-10p <hassan.ali@tenpearls.com>
2016-06-03 11:56:09 +0500

    fruit form and fruit list added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 angular.module('app', [])
   .controller('appCtrl', function($scope){
     $scope.menu = 'Fruit App';
+
+    $scope.fruits = [
+      {"name": "apple", "price": "64"},
+      {"name": "orange", "price": "63"},
+      {"name": "pineApple", "price": "21"}
+    ];
   });
